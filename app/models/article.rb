bb4476e03e61d608655d269777adfe11ddf4ede0
raposojoaopaulo <raposojoaopaulo@gmail.com>
2022-01-25 14:19:27 -0300

    add categories

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,12 @@
 class Article < ApplicationRecord
+  belongs_to :category
+
   validates :title, :presence => true, :length => {:minimum=>3, :maximum=>50}
   validates :body, :presence => true, :length => {:minimum=>10, :maximum=>3999}
 
   paginates_per 4
 
   scope :desc_order, -> { order(created_at: :desc) }
-  scope :without_highlights, -> (ids) { where("id NOT IN(#{ids})") }
+  scope :without_highlights, -> (ids) { where("id NOT IN(#{ids})") if ids.present? }
   # Ex:- scope :active, -> {where(:active => true)}
 end
