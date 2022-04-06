40e70bdeca08ae51eaac1ec42c7a8d5960d83371
raposojoaopaulo <raposojoaopaulo@gmail.com>
2022-04-06 14:27:00 -0300

    adding and config gem bullet

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,10 @@
 class Category < ApplicationRecord
   has_many :articles, dependent: :restrict_with_error
 
-  validates :name, :presence => true, :length => {:minimum=>3, :maximum=>50}
+  validates :name,
+            :presence => true, 
+            :length => {:minimum=>3, :maximum=>50},
+            :uniqueness => {:case_sensitive => false}
 
   scope :sorted, -> { order(:name) }
 end
